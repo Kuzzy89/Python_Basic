321c39b29477bd6bf31667baeee3261296b9e302
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-03-21 07:07:58 +0200

    first steps

---
+++ First Steps/Concetrate data.py
@@ -0,0 +1,6 @@
+architect_name =str(input())
+project_number = int(input())
+
+project_time = project_number * 3
+
+print(f"The architect {architect_name} will need {project_time} hours to complete {project_number} project/s.")
