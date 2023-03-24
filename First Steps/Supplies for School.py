8bb54ccf54ccde3c94c5ae1a4ea18824a72afb92
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-03-24 12:31:23 +0200

    Create Supplies for School.py

---
+++ First Steps/Supplies for School.py
@@ -0,0 +1,10 @@
+pencils = float(input)
+markers = float(input)
+domestos = float(input)
+discount =float(input())
+
+pencils_prise = pencils * 5.8
+markers_price = markers * 7.2
+domestos_price = domestos * 1.2
+
+
