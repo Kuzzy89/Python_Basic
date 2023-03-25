d1c65b7695b634492e763024c9593542de9eaa43
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-03-25 09:01:11 +0200

    basic

---
+++ Fruit or Vegetable.py
@@ -0,0 +1,8 @@
+name = input()
+
+if name in ["banana", "apple", "kiwi", "cherry", "lemon", "grapes"]:
+    print("fruit")
+elif name in ["tomato", "cucumber", "pepper", "carrot"]:
+    print("vegetable")
+else:
+    print("unknown")
