04f977f56f4d40ce6ffc0d290f7fc2cbc27b9abe
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-03-28 07:43:12 +0300

    Create Working Hours.py

---
+++ Working Hours.py
@@ -0,0 +1,10 @@
+hour = int(input())
+day = input()
+
+if 10 <= hour < 18:
+    if day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]:
+        print("open")
+    else:
+        print("closed")
+else:
+    print("closed")
