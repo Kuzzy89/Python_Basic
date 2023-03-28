eaa01b0b35cf0fc31456464b0f6f23d3c861e1e7
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-03-28 07:42:53 +0300

    Create Weekend or Working Day.py

---
+++ Weekend or Working Day.py
@@ -0,0 +1,17 @@
+num = input()
+if num == "Monday":
+    print("Working day")
+elif num == "Tuesday":
+    print("Working day")
+elif num == "Wednesday":
+    print("Working day")
+elif num == "Thursday":
+    print("Working day")
+elif num == "Friday":
+    print("Working day")
+elif num == "Saturday":
+    print("Weekend")
+elif num == "Sunday":
+    print("Weekend")
+else:
+    print("Error")
