37883fbebdb020cd9534fad5dfb015421169f30e
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-04-18 07:13:35 +0300

    Create bg to eu.py

---
+++ bg to eu.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.pack()
+
+app.master.minsize(width=400, height=200)
+    self.pack()
+    self.create_widgets()
+
+def create_widgets(self):
+self.button.bind("<Enter>", self.on_enter)
+self.button.bind("<Leave>", self.on_leave)
