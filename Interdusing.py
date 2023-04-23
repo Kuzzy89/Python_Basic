409c0d24f285f4af9e68ff01625fb0f13d7564f2
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-04-23 09:50:36 +0300

    Create Interdusing.py

---
+++ Interdusing.py
@@ -0,0 +1,9 @@
+first_name = "Stanislav"
+last_name = "Dimitrov"
+age = 32
+town = "Burgas"
+name = first_name + " "\
+       + last_name + ". I am " \
+       + str(age) + "-years old."
+
+print("My name is " + name)
