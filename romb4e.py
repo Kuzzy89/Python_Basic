999de9e978af77fc86040441a74891e4612bf200
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-04-30 07:46:48 +0300

    Create romb4e.py

---
+++ romb4e.py
@@ -0,0 +1,8 @@
+n = int(input())
+print(" " * (n-2) + "* " * (n-2))
+print(" " * (n-3) + "* " * (n-3))
+print(" " * (n-4) + "* " * (n-4))
+# for row in range(1, n + 1):
+#     print(" " * (n-row) + "* " * row)
+# for row in range(1, n):
+#     print(" " * row + "* " * (n-row))
